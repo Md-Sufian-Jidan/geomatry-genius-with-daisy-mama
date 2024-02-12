33990e94f6d9546a8f83f6fae6bb6a65da0ce8a2
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-02-13 00:30:30 +0600

    rectangle calculate complete

--- scripts/rectanglearea.js
+++ scripts/rectanglearea.js
@@ -1,1 +1,19 @@
-console.log('connected')
+// console.log('connected');
+function rectangleCalculate(){
+    const rectangleWidthInput = document.getElementById('rectangle-width')
+    const rectangleWidthText = rectangleWidthInput.value;
+    const Width = parseFloat(rectangleWidthText);
+    // console.log(Width);
+    
+    // get the rectangle length
+    const rectangleLengthInput = document.getElementById('rectangle-length')
+    const rectangleLengthText = rectangleLengthInput.value;
+    const length = parseFloat(rectangleLengthText);
+    // console.log(Length);
+     // Rectangle Area calculate
+     const rectangleArea = Width* length;
+     console.log('total rectangle area is:', rectangleArea);
+
+     const rectangleAreaSpan = document.getElementById('rectangle-area');
+     rectangleAreaSpan.innerText = rectangleArea;
+}
