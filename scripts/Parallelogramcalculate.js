b71b901394090756a4b4fa1e91152d5e3da1306e
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-02-13 14:55:18 +0600

    parallelogram is done

--- scripts/Parallelogramcalculate.js
+++ scripts/Parallelogramcalculate.js
@@ -1,38 +1,12 @@
-// function ParallelogramCalculate(){ const parallelogramBInput = document.getElementById('Parallelogram-base');
-// const ParallelogramBText = parallelogramBInput.value
-// const b = parseFloat(ParallelogramBText);
-// // console.log(b);
-
-// //get the h
-// const ParallelogramHInput = document.getElementById('Parallelogram-height');
-// const ParallelogramHText = ParallelogramHInput.value;
-// const h = parseFloat(ParallelogramHText);
-
-// const ParallelogramArea = b * h;
-// //now display it 
-// const ParallelogramDisplay = document.getElementById('Parallelogram-area');
-// ParallelogramDisplay.innerText = ParallelogramArea;
-// }
-
 // Reusable code making
 
 function calculateParallelogramArea(){
-    const base = getInputValueId('Parallelogram-base');
-    console.log('Parallelogram-base : ',base);
-    const height = getInputValueId('Parallelogram-height');
-    console.log('Parallelogram-height : ',height);
-    const area = base * height;
-    console.log('area of the Parallelogram is : ', area);
-    setInnerTextById('Parallelogram-area',area);
-}
 
-function getInputValueId(inputFieldId){
+
+    const base = getInputValueById('parallelogram-base')
+    console.log(base);
+}   
+function getInputValueById(inputFieldId){
     const inputField = document.getElementById(inputFieldId);
-    const inputValueText = inputField.value
-    const inputValue = parseFloat(inputValueText);
-    return inputValue;
-}
-function setInnerTextById(elementId, area){
-    const element = document.getElementById(elementId);
-    element.innerText = area;
+    console.log(inputField);
 }
